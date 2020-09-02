f176a8e2449f563d1f98c5ae0cbac384499824cc
Strange-Philip <68474949+Strange-Philip@users.noreply.github.com>
2020-09-02 21:22:20 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    string movement;
+    string nutrition;
+    cout << "Please enter yes or no for the following....."
+    cout << "Does the organism move?"
+    cin >> movement;
+    cout << "Does the organism eat?"
+    cin >> nutrition;
+    if(movement== "yes"||nutrition||"yes"){
+        cout<< "Yes  there are extra terrestrial creatures on other planets"
+    }else{
+         cout<< "No there are no extra terrestrial creatures on other planets"
+    }
+    return 0;
+}
